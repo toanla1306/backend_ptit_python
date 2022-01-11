368d5876d125e359488968fa9656dba8d2e9445a
daitech20 <daindp.tech@gmail.com>
2022-01-11 22:19:09 +0700

    first commit

--- exercise_1.py
+++ exercise_1.py
@@ -25,4 +25,33 @@ data = {
 	]
 }
 # Tìm product_1 và product_2 ở cửa hàng nào có mức giá tốt nhất, dữ liệu trả về
-data_return = {"product_1":"store_2", "product_2":"store_3"}
+
+def get_price(x):
+	price = []
+	price.append(int(x[0]["product_1"]["price"]))
+	price.append(int(x[1]["product_2"]["price"]))
+
+	return price
+
+def find_min_product(list_price):
+	store = [0,0]
+
+	product_1 = [x[0] for x in list_price]
+	product_2 = [x[1] for x in list_price]
+
+	min_value_1 = min(product_1)
+	min_value_2 = min(product_2)
+	store[0] = product_1.index(min_value_1) + 1
+	store[1] = product_2.index(min_value_2) + 1
+
+	return store
+
+if __name__ == '__main__':
+	list_price = []
+	store = []
+	for x in data:
+		list_price.append(get_price(data[x]))
+
+	store = find_min_product((list_price))
+	data_return = {"product_1":f"store_{store[0]}", "product_2":f"store_{store[1]}"}
+	print(data_return)
--- exercise_2.py
+++ exercise_2.py
@@ -1,2 +1,32 @@
 # 1. Đọc file sdt_nhan_vien.txt xác định số điện thoại nhập sai
 # 2. Sửa lại số điện thoại bị sai, nếu số lớn hơn 10 thì bỏ chữ số cuối, và số có chữ cái thì bỏ chữ cái đi
+
+def repair_number(number):
+    num = number.split(":")
+    if len(num[0]) <= 10:
+        return num[0] + ':' + num[1]
+    i = 0
+    while len(num[0]) != 10:
+        if i == 10:
+            num[0] = num[0][:10]
+            break
+        if num[0][i] < '0' or num[0][i] > '9':
+            num[0] = num[0].replace(num[0][i], '')
+            continue
+        i+=1
+    return num[0] + ':' + num[1]
+
+def check_number(number):
+    num = repair_number(number)
+    if len(num) != len(number):
+        print("wrong number: " + number)
+    return num
+
+if __name__ == '__main__':
+    f = open("sdt_nhan_vien.txt", "r")
+    list_number = []
+    for x in f:
+        list_number.append(x)
+    for i in range(len(list_number)):
+        list_number[i] = check_number(list_number[i])
+    print(list_number)
