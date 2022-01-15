d67b8d6ad1228316bbaf4990caabc4f9a42b9127
= <=>
2022-01-15 15:51:20 +0700

    update exercise_1 and exercise_2 Toan

--- exercise_1.py
+++ exercise_1.py
@@ -28,4 +28,25 @@ data = {
 
 # Tìm product_1 và product_2 ở cửa hàng nào có mức giá tốt nhất, dữ liệu trả về
 # data_return = {"product_1":"store_2", "product_2":"store_3"}
+def min_price(data, store, type_product, min_price):
+	if len(min_price) == 0:
+		min_price[store] = int(data[type_product]['price'])
+	elif int(list(min_price.values())[0]) > int(data[type_product]['price']):
+		min_price = {}
+		min_price[store] = int(data[type_product]['price'])
+	return min_price
 
+if __name__ == '__main__':
+	min_price_product_1 = {}
+	min_price_product_2 = {}
+	data_return = {}
+	for store, products in data.items():
+		for product in products: 
+			try:
+				min_price_product_1 = min_price(product, store, 'product_1', min_price_product_1)
+			except:
+				min_price_product_2 = min_price(product, store, 'product_2', min_price_product_2)
+
+	data_return['product_1'] = list(min_price_product_1.keys())[0]
+	data_return['product_2'] = list(min_price_product_2.keys())[0]
+	print(data_return)
--- exercise_2.py
+++ exercise_2.py
@@ -1,4 +1,21 @@
 # 1. Đọc file sdt_nhan_vien.txt xác định số điện thoại nhập sai
 # 2. Sửa lại số điện thoại bị sai, nếu số lớn hơn 10 thì bỏ chữ số cuối, và số có chữ cái thì bỏ chữ cái đi
 
+import re
+data = open("sdt_nhan_vien.txt", "r").readlines()
+data_repair = ''
+for index in range(0,len(data)):
+    data_employ = data[index].replace('\n', '').split(':')
+    phone_number = data_employ[0]
+    check_alpha_in_phone_number = re.findall('[a-z]+', phone_number)
+    if len(phone_number) > 10:
+        if len(check_alpha_in_phone_number) > 0:
+            for alpha_in_phone_number in check_alpha_in_phone_number:
+                phone_number = phone_number.replace(alpha_in_phone_number, '')
+        else:
+            phone_number = phone_number[:10]
+        data_employ[0] = phone_number
+    data_repair = data_repair + data_employ[0] + ':' + data_employ[1] + '\n'
 
+file1 = open("sdt_nhan_vien.txt","w")
+file1.write(data_repair)
